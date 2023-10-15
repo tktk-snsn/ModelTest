b1a3733eb3c23af5de783266312f88512afeb695
tktk_snsn <pw_tk.m0t06aro@outlook.com>
2023-10-15 20:38:52 +0900

    Model作成

---
+++ ModelTest/Models/PersonModel.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+
+namespace ModelTest.Models
+{
+    public class PersonModel: ObservableObject
+    {
+        private int _val = 0;
+        public int Val
+        {
+            get => _val;
+            set => SetProperty(ref _val, value);
+        }
+
+        private double _seed = 0.0;
+        public double Seed
+        {
+            get => _seed;
+            set => SetProperty(ref _seed, value);
+        }
+    }
+}
