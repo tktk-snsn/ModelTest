6a537cf9a2bee563be637eb23e9a2417fd3b7cff
tktk_snsn <pw_tk.m0t06aro@outlook.com>
2023-10-15 20:26:58 +0900

    プロジェクト ファイルを追加します。

---
+++ ModelTest/PersonModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelTest
+{
+    public class PersonModel: ObservableObject
+    {
+        private int _val = 0;
+        public int Val
+        {
+            get => _val;
+            set => SetProperty(ref _val, value);
+        }
+
+        private double _seed = 0.0;
+        public double Seed
+        {
+            get => _seed;
+            set => SetProperty(ref _seed, value);
+        }
+    }
+}
