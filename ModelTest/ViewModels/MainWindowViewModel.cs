840fa0a4762468c3dd1b5a9886686702663f18c6
tktk_snsn <pw_tk.m0t06aro@outlook.com>
2023-10-15 20:38:59 +0900

    ViewModel作成

---
+++ ModelTest/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using ModelTest.Models;
+
+
+namespace ModelTest.ViewModels
+{
+    public class MainWindowViewModel: ObservableObject
+    {
+        private PersonModel _model;
+        public PersonModel Model
+        {
+            get => _model;
+            set => SetProperty(ref _model, value);
+        }
+
+
+        private string _message = "default";
+        public string Message
+        {
+            get => _message;
+            set => SetProperty(ref _message, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            _model = new PersonModel
+            {
+                Val = 5,
+                Seed = 2.0,
+            };
+            Model.PropertyChanged += Model_PropertyChanged;
+        }
+
+        private void Model_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            /// sender: PersonModel, e.PropertyName: 変更されたプロパティ名
+            if (e.PropertyName == nameof(Model.Seed))
+                return;
+            UpdateMessage();
+        }
+
+        private void UpdateMessage()
+        {
+            Message = $"Value = {Model.Val}, Seed = {Model.Seed}";
+        }
+    }
+}
